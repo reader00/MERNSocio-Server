97b6c47f860c37b0eb0e7ca79908ab40908a8bbe
reader00 <qoryamanahputra0708@gmail.com>
2023-03-18 16:21:04 +0700

    init

---
+++ models/User.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 64,
+        },
+        lastName: {
+            type: String,
+            required: true,
+            min: 2,
+            max: 64,
+        },
+        email: {
+            type: String,
+            required: true,
+            max: 64,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+        },
+        picturePath: {
+            type: String,
+            default: '',
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        location: String,
+        occupation: String,
+        viewedProfile: Number,
+        impressions: Number,
+    },
+    { timestamps: true }
+);
+
+const User = mongoose.model('User', UserSchema);
+export default User;
